a7939be87b02c4406c87952fb87900d7fe3e4952
Dave Harms <daveharmswebdev@gmail.com>
2016-07-01 10:22:08 -0500

    created specs for weapon explosive and projectile

--- src/specs/weapons_spec.js
+++ src/specs/weapons_spec.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const Weapon = require('../weapon');
+const Explosive = require('../explosive');
+const Projectile = require('../projectile');
+
 describe('specs for weapons', function() {
-	
+	let testWeapon = new Weapon();
+	it('should behave...such that Weapon is defined', function() {
+		expect(testWeapon).toBeDefined();
+	});
 });
 
 describe('specs for explosives', function() {
-	
+	let testExplosive = new Explosive();
+	it('should behave...Explosive is defined', function() {
+		expect(testExplosive).toBeDefined();
+	});
 });
 
 describe('specs for projectiles', function() {
-	
+	let testProjectile = new Projectile();
+	it('should behave...Projectile is defined', function() {
+		expect(testProjectile).toBeDefined();
+	});
 });
