1a9529fccf1c92da80a8d72ba7a34c70cbfc54df
Dave Harms <daveharmswebdev@gmail.com>
2016-07-01 10:09:45 -0500

    finished modesl

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
 'use strict';
 
-const Robot = require('../src/robot');
-const Type = require('../src/type');
-const Model = require('../src/model');
-
-let testRobot = new Robot();
-let testCycle = new Type.Cycle();
-let testTank = new Type.Tank();
-let testJeep = new Type.Jeep();
-let testTornado = new Model.Tornado();
-
-console.log(testRobot);
-console.log(testCycle);
-console.log(testTank);
-console.log(testJeep);
-console.log(testTornado);
---
+++ src/specs/weapons_spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('specs for weapons', function() {
+	
+});
+
+describe('specs for explosives', function() {
+	
+});
+
+describe('specs for projectiles', function() {
+	
+});
