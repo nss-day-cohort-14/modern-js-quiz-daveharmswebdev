332e46fb0a222e2d042a8faed201690b41f0521f
Dave Harms <daveharmswebdev@gmail.com>
2016-07-04 10:38:53 -0500

    fiinished dice module with specs

--- src/dice.js
+++ src/dice.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const Dice = function(sides, dice, percentile) {
+const Dice = function(sides, dice) {
+	var die = () => 1 + Math.floor(Math.random() * this.sides);
+
 	this.dice = dice || 1;
 	this.sides = sides || 6;
-	this.percentile = percentile || false;
+	this.roll = () => {
+		let roll = [];
+		for (let x = 0; x < this.dice; x++) {
+			roll[x] = die();
+		}
+		return roll;
+	};
 };
 
 module.exports = Dice;
--- src/specs/zdice_spec.js
+++ src/specs/zdice_spec.js
@@ -8,7 +8,34 @@ describe('specs for dice', function() {
 		expect(testSix).toEqual(jasmine.objectContaining({
 			dice: 1,
 			sides: 6,
-			percentile: false
 		}));
+	});
+	let testTwoTen = new Dice(10, 2,true);
+	it('can create two 10-sided die for percent', function() {
+		expect(testTwoTen).toEqual(jasmine.objectContaining({
+            dice: 2,
+            sides: 10,
+		}));
+	});
+	it('testSix should return an numbers between 1 and 6', function() {
+		let tenRolls = [];
+		for (let x = 0; x < 10; x++) {
+			tenRolls.push(testSix.roll());			
+		}
+		tenRolls.forEach(function(roll) {
+			expect(roll).toBeLessThan(7);
+		});
+	});
+	it('test20 should return an numbers between 1 and 20', function() {
+		let twentyRolls = [];
+		let testTwenty = new Dice(20,2);
+		for (let x = 0; x < 20; x++) {
+			twentyRolls.push(testTwenty.roll());			
+		}
+		twentyRolls.forEach(function(roll) {
+			roll.forEach(function(die) {
+				expect(die).toBeLessThan(21);
+			});
+		});
 	});
 });
